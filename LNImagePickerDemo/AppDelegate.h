8eb336aab65042048235d4d3d354f76987d11162
onlylin <747325123qq.com>
2016-01-14 23:17:19 +0800

    Initial Commit

---
+++ LNImagePickerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LNImagePickerDemo
+//
+//  Created by 林洁 on 16/1/14.
+//  Copyright © 2016年 onlylin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LNImagePickerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LNImagePickerDemo
+//
+//  Created by 林洁 on 16/1/14.
+//  Copyright © 2016年 onlylin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
